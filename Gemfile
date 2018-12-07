f9499670f73766c769704209c68d2c5a1bb36f52
tomoki.itou <tomoki.itou@super-studio.jp>
2018-12-07 10:41:18 +0900

    Add image posting function

--- Gemfile
+++ Gemfile
@@ -59,3 +59,5 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'dotenv-rails'
 gem 'rails_12factor', group: :production
 gem 'devise'
+gem 'carrierwave'
+gem 'mini_magick'
--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -15,7 +15,15 @@ class BlogsController < ApplicationController
 
   # GET /blogs/new
   def new
-    @blog = Blog.new
+    if params[:back]
+      @blog = Blog.new(blog_params)
+    else
+      @blog = Blog.new
+    end
+  end
+
+  def confirm
+    @blog = Blog.new(blog_params)
   end
 
   # GET /blogs/1/edit
@@ -71,6 +79,6 @@ class BlogsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def blog_params
-      params.require(:blog).permit(:title, :content)
+      params.require(:blog).permit(:title, :content, :image, :image_cache)
     end
 end
