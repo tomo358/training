10d9ae9f922f3c5f0fc4a9a3e207344a98c5a24b
tomoki.itou <tomoki.itou@super-studio.jp>
2019-01-21 10:40:11 +0900

    Addition user_index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
   root to: "blogs#index"
   resources :blogs
+  devise_for :users
   resources :favorites, only: [:create, :destroy, :index]
+  resources :users, only: [:index]
 
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
