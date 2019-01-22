928785464acf7e49c03d6f74aa85b95dfa7f30bd
tomoki.itou <tomoki.itou@super-studio.jp>
2019-01-22 15:55:23 +0900

    return

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   get 'relationships/create'
-
   get 'relationships/destroy'
 
   root to: "blogs#index"
@@ -8,13 +7,6 @@ Rails.application.routes.draw do
   devise_for :users
   resources :favorites, only: [:create, :destroy, :index]
   resources :users, only: [:index]
-
-  get '/user', to: 'users#user'
-  post '/user', to: 'users#user'
-
-  get 'select_user', to: 'users#select_user'
-  post 'select_user', to: 'users#select_user'
-
   resources :relationships, only: [:create, :destroy]
 
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
